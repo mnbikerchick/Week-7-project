165d4e30c112e73b2d54aab815995d765f1c1911
Jeanne Olsen <Jeanne.Olsen@my.strayer.edu>
2022-11-20 21:10:23 -0600

    week 7 ask for help

--- Week_7_project.py
+++ Week_7_project.py
@@ -1,1 +1,117 @@
+# Jeanne K Olsen
+# CIS 261 Week 7
+# Course Project
+from datetime import datetime
 
+def GetEmpName():
+    empname = input("Enter employee name: ")
+    return empname
+def GetDatesWorked():
+    fromdate = input("Enter Start Date (mm/dd/yyyy): ")
+    todate = input("Enter End Date (mm/dd/yyyy): ")
+    return fromdate, todate
+def GetHoursWorked():
+    hours = float(input('Enter amount of hours worked:  '))
+    return hours
+def GetHourlyRate():
+    hourlyrate = float(input ("Enter hourly rate: "))
+    return hourlyrate
+def GetTaxRate():
+    taxrate = float(input ("Enter tax rate: "))
+    return taxrate
+def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
+    grosspay = hours * hourlyrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
+
+def printinfo(DetailsPrinted):
+    TotEmployees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+
+    file = open('employees.txt', 'r') 
+    EmpFile = f.read()
+    
+while True:
+        rundate = input ("Enter start date for report (MM/DD/YYYY) or All for all data in file: ")
+        if (rundate.upper() == "ALL"):
+            break
+        try:
+            rundate = datetime.strptime(rundate, "%m/%d/%Y")
+            break
+        except ValueError:
+            print("Invalid date format. Try again.")
+            print()
+            continue  # skip next if statement and re-start loop
+        while True:
+            with open('empFile.txt' ,'r') as f:
+                EmpDetail = f.read()
+        
+        if not EmpDetail:
+            break
+        EmpFile = file.readlines()
+        modified = []
+        for line in EmpFile:
+            modified.append(line.strip())
+        EmpFile.split('|')
+        Empfile = EmpList
+        fromdate = EmpList[0]
+        if (str(rundate).upper() != "ALL"):
+            checkdate = datetime.strptime(fromdate, "%m/%d/%Y")
+            if (checkdate < rundate):
+                continue        
+        todate = EmpList[1]
+        empname = EmpList[2]
+        hours = float(EmpList[3])
+        hourlyrate  = float(EmpList[4])
+        taxrate = float(EmpList[5])
+        grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
+        print(fromdate, todate, empname, f"{hours:,.2f}",  f"{hourlyrate:,.2f}", f"{grosspay:,.2f}",  f"{taxrate:,.1%}",  f"{incometax:,.2f}",  f"{netpay:,.2f}")
+        TotEmployees += 1
+        TotHours += hours
+        TotGrossPay += grosspay
+        TotTax += incometax
+        TotNetPay += netpay
+        EmpTotals["TotEmp"] = TotEmployees
+        EmpTotals["TotHrs"] = TotHours
+        EmpTotals["TotGrossPay"] = TotGrossPay
+        EmpTotals["TotTax"] = TotTax
+        EmpTotals["TotNetPay"] = TotNetPay
+        DetailsPrinted = True
+        if (DetailsPrinted):  #skip of no detail lines printed
+            PrintTotals (EmpTotals)
+        else:
+            print("No detail information to print")
+def PrintTotals(EmpTotals):    
+    print()
+    print(f'Total Number Of Employees: {EmpTotals["TotEmp"]}')
+    print(f'Total Hours Worked: {EmpTotals["TotHrs"]:,.2f}')
+    print(f'Total Gross Pay: {EmpTotals["TotGrossPay"]:,.2f}')
+    print(f'Total Income Tax:  {EmpTotals["TotTax"]:,.2f}')
+    print(f'Total Net Pay: {EmpTotals["TotNetPay"]:,.2f}')   
+
+if __name__ == "__main__":
+      with open('Employees.txt','a'):
+
+        DetailsPrinted = False
+      while True:
+        empname = GetEmpName()
+        if (empname.upper() == "END"):
+                break
+        fromdate, todate = GetDatesWorked()
+        hours = GetHoursWorked()
+        hourlyrate = GetHourlyRate()
+        taxrate = GetTaxRate()
+        ##############################################################
+        # write the line of code that will concatenate fromdate, todate, empname, hours, hourlyrate, and taxrate. Pipe delimit each value and add a carriage return to the end of the line
+ 
+ # and assign the line to EmpDetail
+EmpDetail = [fromdate, todate, empname, hours, hourlyrate, taxrate]
+EmpDetail = EmpFile
+        # write the liie of code that will write EmpDetail to EmpFile
+   # write the line of code to close EmpFile
+fileclose(EmpFile)
+printinfo(DetailsPrinted)
